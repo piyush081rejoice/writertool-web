c3c1a9a6cb9741668b2287752d7ee12e3833b7f8
keyurdevani <132330039+SmitKakani7@users.noreply.github.com>
2024-07-13 12:39:39 +0530

    remove all path in middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,18 +1,17 @@
 import { NextResponse } from "next/server";
 export function middleware(request) {
-
   const currentPath = request.nextUrl.pathname;
 
   const userToken = request.cookies.has("userToken");
 
   const protectedRoutes = ["/write-blog", "/your-stories"];
-
+  console.log(` outside1`, request.cookies.has("userToken"));
   if (protectedRoutes.includes(currentPath) && !request.cookies.has("userToken")) {
+    console.log(`inner`, request.cookies.has("userToken"));
     return NextResponse.redirect(new URL(`/sign-up`, request.url));
   }
   return NextResponse.next();
-
 }
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/write-blog","/your-stories"],
 };
