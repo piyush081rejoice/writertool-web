483bb868767bfb080846d66b66ec8918b974f063
keyurdevani <132330039+SmitKakani7@users.noreply.github.com>
2024-07-13 11:41:45 +0530

    remove conditions

--- src/shared/components/header/index.js
+++ src/shared/components/header/index.js
@@ -1,46 +1,39 @@
-import UserIcon from '@/assets/icons/userIcon';
-import WriteIcon from '@/assets/icons/writeIcon';
-import { getCookie } from '@/hooks/useCookie';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
-import ProfileSidebar from '../profileSidebar';
-import styles from './header.module.scss';
-const WriterTools = '/assets/logo/logo.svg';
-const NotificationIcon = '/assets/icons/notification.svg';
-const VectoreIcon = '/assets/icons/vectore.svg';
+import UserIcon from "@/assets/icons/userIcon";
+import WriteIcon from "@/assets/icons/writeIcon";
+import { getCookie } from "@/hooks/useCookie";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import ProfileSidebar from "../profileSidebar";
+import styles from "./header.module.scss";
+const WriterTools = "/assets/logo/logo.svg";
+const NotificationIcon = "/assets/icons/notification.svg";
+const VectoreIcon = "/assets/icons/vectore.svg";
 const Header = () => {
   const [sidebar, setSidebar] = useState(false);
   const [userLoggedIn, setUserLoggedIn] = useState(false);
   const router = useRouter();
-  const handleWriteBlog =()=>{
-    const userLogin =getCookie("userToken")
-    console.log("🚀 ~ file: index.js:18 ~ handleWriteBlog ~ userLogin:", userLogin)
-    if (userLogin !== undefined) {
-      router.push("/write-blog")
-    }else{
-      router.push("/sign-in")
-    }
-
-  }
+  const handleWriteBlog = () => {
+    console.log(`HEllo`)
+    router.push("/write-blog");
+  };
   useEffect(() => {
     const userEmail = getCookie("userToken");
     if (userEmail == undefined) {
       setUserLoggedIn(false);
-    }else{
+    } else {
       setUserLoggedIn(true);
     }
-    
   }, [getCookie("userToken")]);
 
   return (
     <>
       <header className={styles.header}>
-        <div className='container'>
+        <div className="container">
           <div className={styles.headerAlignment}>
             <div className={styles.logo}>
               <Link href="/">
-              <img src={WriterTools} alt='WriterTools' width='100%' height='100%' />
+                <img src={WriterTools} alt="WriterTools" width="100%" height="100%" />
               </Link>
             </div>
             <div className={styles.rightAllContent}>
@@ -48,31 +41,30 @@ const Header = () => {
                 <WriteIcon />
                 Write
               </button>
-            
-              {
-                !userLoggedIn &&
-                  <button onClick={()=>router.push("/sign-in")} className={styles.outline}>
-                    <UserIcon />
-                    Sign In
-                  </button>
-              }
+
+              {!userLoggedIn && (
+                <button onClick={() => router.push("/sign-in")} className={styles.outline}>
+                  <UserIcon />
+                  Sign In
+                </button>
+              )}
               <div className={styles.notificationIcon}>
                 <Link href="/notifications">
-                <img width='100%' height='100%' src={NotificationIcon} alt='NotificationIcon' />
+                  <img width="100%" height="100%" src={NotificationIcon} alt="NotificationIcon" />
                 </Link>
               </div>
-              {userLoggedIn &&
-              <div className={styles.profileImage} onClick={() => setSidebar(!sidebar)}>
-                <img width='100%' height='100%' src={VectoreIcon} alt='VectoreIcon' />
-              </div>
-              }
+              {userLoggedIn && (
+                <div className={styles.profileImage} onClick={() => setSidebar(!sidebar)}>
+                  <img width="100%" height="100%" src={VectoreIcon} alt="VectoreIcon" />
+                </div>
+              )}
             </div>
           </div>
         </div>
       </header>
       <ProfileSidebar sidebar={sidebar} setSidebar={setSidebar} />
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
