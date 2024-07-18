aea944875ef9831e54830874f1022636069230d3
keyurdevani <132330039+SmitKakani7@users.noreply.github.com>
2024-07-18 13:59:34 +0530

    update req

--- src/middleware.js
+++ src/middleware.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 const protectedRoutes = ["/write-blog", "/your-stories", "/category/for-you", "/library", "/profile-setting"];
 
 export default function middleware(req) {
-  const isAuthenticated = request.cookies.has("userToken");
+  const isAuthenticated = req.cookies.has("userToken");
   if (!isAuthenticated && protectedRoutes.includes(req.nextUrl.pathname)) {
     const absoluteURL = new URL("/sign-in", req.nextUrl.origin);
     return NextResponse.redirect(absoluteURL.toString());
