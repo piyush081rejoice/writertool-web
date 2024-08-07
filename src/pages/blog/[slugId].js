eac4b6ef0dbba56c01600c6347e3a645bcc0f3b6
keyurdevani <keyur289.rejoice@gmail.com>
2024-08-07 18:05:09 +0530

    added seo

--- src/pages/blog/[slugId].js
+++ src/pages/blog/[slugId].js
@@ -1,15 +1,19 @@
 import { ApiGet } from "@/helpers/API/ApiData";
-import BlogInside from "@/module/blogInside";
 import { useRouter } from "next/router";
-
-const UpdateBlog = ({ getSingleBlogData ,isTrendingBlogsData }) => {
+import dynamic from "next/dynamic";
+const BlogInside = dynamic(() => import("@/module/blogInside"));
+const NextSEO = dynamic(() => import("@/common/NextSeo"));
+const UpdateBlog = ({ getSingleBlogData, isTrendingBlogsData, seoData }) => {
   const router = useRouter();
   const { slugId } = router.query;
 
   return (
-    <div>
-      <BlogInside slugId={slugId} singleBlog={getSingleBlogData} isTrendingBlogsData={isTrendingBlogsData} />
-    </div>
+    <>
+      <NextSEO seo={seoData} />
+      <div>
+        <BlogInside slugId={slugId} singleBlog={getSingleBlogData} isTrendingBlogsData={isTrendingBlogsData} />
+      </div>
+    </>
   );
 };
 
@@ -17,16 +21,19 @@ export default UpdateBlog;
 
 export async function getServerSideProps(context) {
   const { slugId } = context.params;
-  
 
   try {
     const singleBlogData = await ApiGet(`blog-services/blogs/get?slugId=${slugId}`).then((resp) => resp?.data?.payload);
     const isTrendingBlogsData = await ApiGet("blog-services/blogs/get?isTrending=true&skip=1&limit=3").then((resp) => resp?.data?.payload?.blogs);
-  
+    const seoData = {
+      Title: `${singleBlogData?.title} | WriterTools`,
+      Description: singleBlogData?.sortDescription || "Read this insightful blog post on WriterTools.",
+    };
     return {
       props: {
         getSingleBlogData: singleBlogData?.blogs,
         isTrendingBlogsData: isTrendingBlogsData || [],
+        seoData: seoData || null,
       },
     };
   } catch (error) {
