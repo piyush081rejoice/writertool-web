f2555e181c381a671782076e672a23a1845c0594
keyurdevani <keyur289.rejoice@gmail.com>
2024-08-07 16:54:01 +0530

    added padding

--- src/pages/otp-verify/index.js
+++ src/pages/otp-verify/index.js
@@ -1,7 +1,32 @@
-import OtpVerificationModal from "@/shared/components/otpVerificationModal";
-
-const otpVerify = () => {
-  return <OtpVerificationModal />;
+import dynamic from "next/dynamic";
+const OtpVerificationModal = dynamic(() => import("@/shared/components/otpVerificationModal"));
+const NextSEO = dynamic(() => import("@/common/NextSeo"));
+const otpVerify = ({ seoData }) => {
+  return (
+    <>
+      <NextSEO seo={seoData} />
+      <OtpVerificationModal />
+    </>
+  );
 };
 
 export default otpVerify;
+export async function getServerSideProps() {
+  try {
+    const seoData = {
+      Title: "OTP Verification | WriterTools",
+      Description: "Verify your email address with the OTP sent to your inbox to complete the sign-up process and access WriterTools.",
+    };
+    return {
+      props: {
+        seoData: seoData || null,
+      },
+    };
+  } catch (error) {
+    return {
+      props: {
+        error: "Facing error",
+      },
+    };
+  }
+}
