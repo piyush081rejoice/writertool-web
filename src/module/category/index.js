641fcf89a7311e1b37ca29433964fb9d0df61e43
keyurdevani <keyur289.rejoice@gmail.com>
2024-08-08 15:42:20 +0530

    comment slider

--- src/module/category/index.js
+++ src/module/category/index.js
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 import dynamic from "next/dynamic";
 import styles from "./category.module.scss";
-const Tab = dynamic(() => import("@/shared/components/tab"));
+// const Tab = dynamic(() => import("@/shared/components/tab"));
 const CategoryTopbar = dynamic(() => import("./categoryTopbar"));
 const CategoryInformation = dynamic(() => import("./categoryInformation"));
 
 export default function Category({ isTrendingBlogsData, getBlogCategoryData, slugId }) {
   return (
     <div>
-      <div className={styles.tabSpacer}>
+      {/* <div className={styles.tabSpacer}>
         <Tab {...{ getBlogCategoryData }} />
-      </div>
+      </div> */}
       <CategoryTopbar {...{ slugId }} />
       <CategoryInformation {...{ slugId, isTrendingBlogsData }} />
     </div>
